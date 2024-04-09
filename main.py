551fe2fbb84276791d8ae7418b8a1c71ddae3415
Diana Yarmukhametova <diana.yarmykh@list.ru>
2024-04-09 19:04:41 +0300

    abuba

--- main.py
+++ main.py
@@ -16,31 +16,32 @@ def mainpage():
     form = MainForm()
     if request.method == 'POST':
         function = request.form['function']  # так берётся текст введенной функции
-        fig = Figure()
-        curve = fig.add_subplot(1, 1, 1)
-        graph_dot_count = 100
-        func_value = [0] * graph_dot_count
-        args_value = [0] * graph_dot_count
-        x_start = 0
-        x_end = 10
-        x_delta = (x_end - x_start) / graph_dot_count
-        x = x_start
-        for i in range(graph_dot_count):
-            func_value[i] = eval(function)
-            args_value[i] = x
-            x += x_delta
+        if function:
+            fig = Figure()
+            curve = fig.add_subplot(1, 1, 1)
+            graph_dot_count = 100
+            func_value = [0] * graph_dot_count
+            args_value = [0] * graph_dot_count
+            x_start = 0
+            x_end = 10
+            x_delta = (x_end - x_start) / graph_dot_count
+            x = x_start
+            for i in range(graph_dot_count):
+                func_value[i] = eval(function)
+                args_value[i] = x
+                x += x_delta
 
-        curve.plot(args_value, func_value, "ro-")
+            curve.plot(args_value, func_value, "ro-")
 
-        # Convert plot to PNG image
-        pngImage = io.BytesIO()
-        FigureCanvas(fig).print_png(pngImage)
+            # Convert plot to PNG image
+            pngImage = io.BytesIO()
+            FigureCanvas(fig).print_png(pngImage)
 
-        # Encode PNG image to base64 string
-        pngImageB64String = "data:image/png;base64,"
-        pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+            # Encode PNG image to base64 string
+            pngImageB64String = "data:image/png;base64,"
+            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
-        return render_template("main.html", title=function, image=pngImageB64String, form=form)
+            return render_template("main.html", title=function, image=pngImageB64String, form=form)
     return render_template("main.html", image=None, form=form)
 
 if __name__ == '__main__':
