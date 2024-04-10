103cc4c304fde78741a870dc46a8c012ae55170a
sanya <ketokodama@gmail.com>
2024-04-10 20:18:35 +0600

    calc_fft

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import render_template, Flask, request
 import io
 import base64
+import calc_fft
 
+from flask import render_template, Flask, request
 from forms.main_form import MainForm
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from math import *
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -33,15 +35,28 @@ def mainpage():
 
             curve.plot(args_value, func_value, "ro-")
 
+            spectre_fig = Figure()
+            spectre = spectre_fig.add_subplot(1, 1, 1)
+            y = calc_fft.calc_fft(func_value)
+
+            spectre.plot(abs(y), "ro-")
+
+
             # Convert plot to PNG image
             pngImage = io.BytesIO()
             FigureCanvas(fig).print_png(pngImage)
+
+            pngImage_spectre = io.BytesIO()
+            FigureCanvas(spectre_fig).print_png(pngImage_spectre)
 
             # Encode PNG image to base64 string
             pngImageB64String = "data:image/png;base64,"
             pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
-            return render_template("main.html", title=function, image=pngImageB64String, form=form)
+            pngImage_spectreB64String = "data:image/png;base64,"
+            pngImage_spectreB64String += base64.b64encode(pngImage_spectre.getvalue()).decode('utf8')
+
+            return render_template("main.html", title=function, image=pngImageB64String, image_spectre=pngImage_spectreB64String, form=form)
     return render_template("main.html", image=None, form=form)
 
 if __name__ == '__main__':
