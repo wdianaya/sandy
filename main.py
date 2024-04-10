0c2e1e3b670ff51153241ed8893adcdb50ffd5a3
Diana Yarmukhametova <diana.yarmykh@list.ru>
2024-04-10 18:14:44 +0300

    кнопки

--- main.py
+++ main.py
@@ -17,52 +17,54 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def mainpage():
     form = MainForm()
     if request.method == 'POST':
-        try:
-            function = request.form['function']  # так берётся текст введенной функции
-            if function:
-                fig = Figure()
-                curve = fig.add_subplot(1, 1, 1)
-                graph_dot_count = 100
-                func_value = [0] * graph_dot_count
-                args_value = [0] * graph_dot_count
-                x_start = 0
-                x_end = 10
-                x_delta = (x_end - x_start) / graph_dot_count
-                x = x_start
-                for i in range(graph_dot_count):
-                    try:
-                        func_value[i] = eval(function)
-                    except ZeroDivisionError:
-                        pass
-                    args_value[i] = x
-                    x += x_delta
+        if not all(key in request.form for key in
+                 ['function', 'start_dot', 'end_dot', 'count']):
+            abort(400)
+        function = request.form['function']  # текст введенной функции
+        start_dot = request.form['start_dot']  # начальная
+        end_dot = request.form['end_dot']  # конечная
+        amount = request.form['count']  # количество точек
+        fig = Figure()
+        curve = fig.add_subplot(1, 1, 1)
+        graph_dot_count = 100
+        func_value = [0] * graph_dot_count
+        args_value = [0] * graph_dot_count
+        x_start = 0
+        x_end = 10
+        x_delta = (x_end - x_start) / graph_dot_count
+        x = x_start
+        for i in range(graph_dot_count):
+            try:
+                func_value[i] = eval(function)
+            except ZeroDivisionError:
+                pass
+            args_value[i] = x
+            x += x_delta
 
-                curve.plot(args_value, func_value, "ro-")
+        curve.plot(args_value, func_value, "ro-")
 
-                spectre_fig = Figure()
-                spectre = spectre_fig.add_subplot(1, 1, 1)
-                y = calc_fft.calc_fft(func_value)
+        spectre_fig = Figure()
+        spectre = spectre_fig.add_subplot(1, 1, 1)
+        y = calc_fft.calc_fft(func_value)
 
-                spectre.plot(abs(y), "ro-")
+        spectre.plot(abs(y), "ro-")
 
 
-                # Convert plot to PNG image
-                pngImage = io.BytesIO()
-                FigureCanvas(fig).print_png(pngImage)
+        # Convert plot to PNG image
+        pngImage = io.BytesIO()
+        FigureCanvas(fig).print_png(pngImage)
 
-                pngImage_spectre = io.BytesIO()
-                FigureCanvas(spectre_fig).print_png(pngImage_spectre)
+        pngImage_spectre = io.BytesIO()
+        FigureCanvas(spectre_fig).print_png(pngImage_spectre)
 
-                # Encode PNG image to base64 string
-                pngImageB64String = "data:image/png;base64,"
-                pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+        # Encode PNG image to base64 string
+        pngImageB64String = "data:image/png;base64,"
+        pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
-                pngImage_spectreB64String = "data:image/png;base64,"
-                pngImage_spectreB64String += base64.b64encode(pngImage_spectre.getvalue()).decode('utf8')
+        pngImage_spectreB64String = "data:image/png;base64,"
+        pngImage_spectreB64String += base64.b64encode(pngImage_spectre.getvalue()).decode('utf8')
 
-                return render_template("main.html", title=function, image=pngImageB64String, image_spectre=pngImage_spectreB64String, form=form)
-        except Exception:
-            raise Exception
+        return render_template("main.html", title=function, image=pngImageB64String, image_spectre=pngImage_spectreB64String, form=form)
     return render_template("main.html", image=None, form=form)
 
 if __name__ == '__main__':
