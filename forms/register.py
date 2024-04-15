0d2cef7cdd0dc79f9cc84456a27cfba4e5943870
Diana Yarmukhametova <diana.yarmykh@list.ru>
2024-04-15 16:56:56 +0300

    register login

---
+++ forms/register.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Login/email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import io
 import base64
 import calc_fft
-
 from data import db_session
-from flask import render_template, Flask, request
+from forms.login import LoginForm
+from forms.register import RegisterForm
+from data.users import User
+from flask import Flask, render_template, redirect, request, make_response, abort, jsonify
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from forms.main_form import MainForm
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -13,17 +16,32 @@ import mpld3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 def main():
-    # db_session.global_init("db/sandy.db")
-    # db_sess = db_session.create_session()
+    db_session.global_init("db/sandy.db")
+    db_sess = db_session.create_session()
     app.run(port=8000, host='127.0.0.1')
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template("base.html")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route("/graf", methods=['GET', 'POST'])
@@ -76,6 +94,62 @@ def mainpage():
         except Exception:
             raise Exception
     return render_template("main.html", image=None, form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            username=form.username.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Password or login is incorrect",
+                               form=form)
+    return render_template('login.html', form=form)
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
 
 if __name__ == '__main__':
     main()
